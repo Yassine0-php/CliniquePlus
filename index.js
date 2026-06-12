331e5c2a38e42f1b9e589b3ffe5afb110355d69f
yassine Bessaim <yassinebessaim@gmail.com>
2026-06-12 14:37:40 +0200

    ajouterUnPatient

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ const patientRoute = require('./routes/patient.js')
 
 app.use('/login', userRoute)
 app.use('/patientId', patientRoute)
+app.use('/patients',patientRoute)
 
 // On connecte la base de données
-let bddCliniquePlus = new sqlite3.Database('./CliniquePlus.db', sqlite3.OPEN_READONLY, (err) => {
+let bddCliniquePlus = new sqlite3.Database('./CliniquePlus.db', sqlite3.OPEN_READWRITE, (err) => {
     if(err) {
         console.error(err.message);
     } else {
@@ -26,13 +27,30 @@ let bddCliniquePlus = new sqlite3.Database('./CliniquePlus.db', sqlite3.OPEN_REA
 });
 
 
-// Déclaration de la route et du traitement des données POST
+// ajouter un patient
+
+// app.post("/patients", (req, res) => {
+//     const { nom, prenom, age, mail, telephone } = req.body;
+
+//     const sql = "INSERT INTO patient (nom, prenom, age, mail, telephone) VALUES (?, ?, ?,?,?)";
+
+//     bddCliniquePlus.run(sql, [nom, prenom, age, mail, telephone], function (err) {
+//         if (err) {
+//             return res.status(500).json({ error: err.message });
+//         }
+
+//         res.status(201).json({
+//             message: "Patient ajouté avec succès",
+//             id: this.lastID
+//         });
+//     });
+// });
 
 
 
 
 
-app.use(router);
+
 
 // On initialise le serveur et son port d'écoute
 app.listen(3000, () => {
