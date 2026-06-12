98de818d37f87cc3431d24cbb684d105f7cf0c78
yassine Bessaim <yassinebessaim@gmail.com>
2026-06-12 11:24:47 +0200

    creationDossierRoute

---
+++ routes/patient.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get('/patientId', (req,res) => {
+
+    const { id } = req.body
+    console.log("id reçu : ", id)
+    patientModele.findPatientById(id, (err, patient) => {
+        console.log(patient)
+        if (err) {
+            return res.status(500).json({ success: false });
+        }
+
+        if (patient) {
+             return res.status(200).json({
+                "patient": {
+                    "id": patient.id,
+                    "nom": patient.nom,
+                    "prenom": patient.prenom,
+                    "mail": patient.mail,
+                    
+                }
+             });
+        
+        }else {
+             return res.status(404).json({"message":"Patient non trouvé"})
+        }
+
+            
+    });
+});
+
+module.exports = router;
